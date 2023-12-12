461cc0878a0af4a0cf8b22ca2eb75f6fe04e7a89
Jonathan Brierley <jonnywb@users.noreply.github.com>
2023-12-12 22:06:48 +0000

    add user, account and context in prep for postComment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import app from "./App.module.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Articles from "./components/Articles/Articles";
+import Account from "./components/Account/Account";
+import { UserProvider } from "./components/Context/UserProvider";
 
 function App() {
   return (
@@ -12,7 +14,12 @@ function App() {
         <Nav />
       </header>
       <main>
-        <Articles />
+        <UserProvider>
+          <Routes>
+            <Route path="*" element={<Articles />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </UserProvider>
       </main>
     </>
   );
